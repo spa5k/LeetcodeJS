935d8126faa08ef32d6318443f81e95603f3920d
Spark <79936503+spa5k@users.noreply.github.com>
2023-04-08 06:53:08 +0530

    more questions

--- src/1.two-sum.js
+++ src/1.two-sum.js
@@ -1,3 +1,10 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -16,5 +23,9 @@ var twoSum = function (nums, target) {
   }
   return [];
 };
+// @lc code=end
+console.log(twoSum([2, 7, 11, 15], 9));
 
-twoSum([2, 7, 11, 15], 9);
+// @after-stub-for-debug-begin
+module.exports = twoSum;
+// @after-stub-for-debug-end
