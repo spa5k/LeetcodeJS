9b716af56c4820a3b8fcb10f7455db83910b2bf2
Spark <79936503+spa5k@users.noreply.github.com>
2024-07-10 15:21:09 +0000

    more questions

---
+++ src/139.word-break.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=139 lang=javascript
+ *
+ * [139] Word Break
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {
+    const memo = {};
+
+    function dfs(startIndex) {
+        if (startIndex === s.length) return true;
+
+        if (startIndex in memo) return memo[startIndex];
+
+        let ans = false;
+        for (const word of wordDict) {
+            if (s.slice(startIndex).startsWith(word)) {
+                if (dfs(startIndex + word.length)) {
+                    ans = true;
+                    break;
+                }
+            }
+        }
+        memo[startIndex] = ans;
+        return ans;
+    }
+
+    return dfs(0);
+};
+// @lc code=end
---
+++ src/322.coin-change.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=322 lang=javascript
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+
+function coinChange(coins, amount) {
+    let memo = new Array(amount + 1).fill(-1);
+
+    function dfs(sum) {
+        if (sum === amount) {
+            return 0;
+        }
+        if (sum > amount) {
+            return Infinity;
+        }
+        if (memo[sum] !== -1) {
+            return memo[sum];
+        }
+        let ans = Infinity;
+        for (let coin of coins) {
+            let result = dfs(sum + coin);
+            if (result !== Infinity) {
+                ans = Math.min(ans, result + 1);
+            }
+        }
+        return (memo[sum] = ans);
+    }
+
+    let result = dfs(0);
+    return result === Infinity ? -1 : result;
+}
+
+// @lc code=end
