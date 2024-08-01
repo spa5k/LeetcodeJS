9a6e4978eac743ecbafc792c26c9bc6f93511e92
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-01 17:18:47 +0000

    more questions

--- src/62.unique-paths.js
+++ src/62.unique-paths.js
@@ -4,11 +4,31 @@
  * [62] Unique Paths
  */
 
-// @lc code=start
 /**
  * @param {number} m
  * @param {number} n
  * @return {number}
+ */
+var uniquePathsRecursion = function (m, n, memo = {}) {
+    const key = m + ',' + n;
+    if (memo[key]) return memo[key];
+    if (m === 0 || n === 0) return 0;
+
+    if (m === 1 && n === 1) return m * n;
+
+    const res = uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo);
+    memo[key] = res;
+    return res;
+};
+
+// @lc code=start
+
+/**
+ * Calculates the number of unique paths from the top-left corner to the bottom-right corner of a grid.
+ * @param {number} m - The number of rows in the grid.
+ * @param {number} n - The number of columns in the grid.
+ * @param {Object} memo - An optional memoization object to store previously calculated results.
+ * @returns {number} The number of unique paths.
  */
 var uniquePaths = function (m, n) {
     const dp = [];
