898f24d73d4b85f2b6bae6391bc4c767cdf5781a
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-02 15:04:26 +0000

    more questions

---
+++ src/494.target-sum.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=494 lang=javascript
+ *
+ * [494] Target Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+
+var findTargetSumWays = function (nums, target) {
+    const memo = {};
+    function countWays(index, currentSum) {
+        const key = `${index}:${currentSum}`;
+
+        if (key in memo) {
+            return memo[key];
+        }
+
+        if (index === nums.length) {
+            return currentSum == target ? 1 : 0;
+        }
+
+        const add = countWays(index + 1, currentSum + nums[index]);
+        const subtract = countWays(index + 1, currentSum - nums[index]);
+        const result = add + subtract;
+        memo[key] = result;
+        return result;
+    }
+
+    return countWays(0, 0);
+};
+// @lc code=end
