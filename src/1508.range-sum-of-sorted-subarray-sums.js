c59dbdde60e216f2cf1052701d75eddd8a47a253
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-04 17:53:27 +0000

    more questions

---
+++ src/1508.range-sum-of-sorted-subarray-sums.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1508 lang=javascript
+ *
+ * [1508] Range Sum of Sorted Subarray Sums
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeSum = function (nums, n, left, right) {
+    const MOD = 1e9 + 7;
+    const arr = [];
+
+    for (let i = 0; i < n; i++) {
+        arr.push(nums[i]);
+        let last = nums[i];
+        for (let j = i + 1; j < n; j++) {
+            arr.push(last + nums[j]);
+            last = last + nums[j];
+        }
+    }
+
+    arr.sort((a, b) => a - b);
+
+    let sum = 0;
+    for (let i = left - 1; i < right; i++) {
+        // Just do modulo here, no need to manage it elsewhere, otherwise the result gets too big.
+        // Javascript can handle it, but not other languages.
+        sum = (sum + arr[i]) % MOD;
+    }
+    return sum;
+};
+// @lc code=end
